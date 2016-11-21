a515319bcc4240ea719e44e90e1b074bd39ee13f
Warren Schweigert <warren.sch.441@gmail.com>
2016-11-21 13:14:22 -0700

    fractal evaluator for procedural generation

---
+++ src/FractalExspressionEvaluator.cpp
@@ -0,0 +1,49 @@
+#include "FractalExspressionEvaluator.h"
+
+FractalExspressionEvaluator::FractalExspressionEvaluator():
+    mEscape(500),
+    mMaxIters(512),
+    mJuliaValue(-.621,0),
+    mRefSize(512),
+    mRefPoit1(-.375,.5),
+    mRefPoit2(-.125,.25)
+{
+
+}
+
+int FractalExspressionEvaluator::getEscapeValue(){return mEscape;}
+void FractalExspressionEvaluator::setEscapeValue(unsigned int Escape){mEscape=Escape;}
+
+int FractalExspressionEvaluator::getMaxIters(){return mMaxIters;}
+void FractalExspressionEvaluator::setMaxIters(unsigned int MaxIters){mMaxIters=MaxIters;}
+
+int FractalExspressionEvaluator::GetRefSize(){return mRefSize;}
+void FractalExspressionEvaluator::setRefSize(unsigned int RefSize){mRefSize=RefSize;}
+
+std::complex<double> FractalExspressionEvaluator::getJuliaValue(){return mJuliaValue;}
+void FractalExspressionEvaluator::setJuliaValue(std::complex<double> JuliaValue){mJuliaValue=JuliaValue;}
+
+std::complex<double> FractalExspressionEvaluator::getRefPoit1(){return mRefPoit1;}
+void FractalExspressionEvaluator::setRefPoit1(std::complex<double> RefPoit1){mRefPoit1=RefPoit1;}
+
+std::complex<double> FractalExspressionEvaluator::getRefPoit2(){return mRefPoit2;}
+void FractalExspressionEvaluator::setRefPoit2(std::complex<double> RefPoit2){mRefPoit2=RefPoit2;}
+
+
+int FractalExspressionEvaluator::getValue(double x,double y)
+{
+
+    //correct for position
+    x = mRefPoit1.real() + x*(mRefPoit2.real() - mRefPoit1.real())/mRefSize;
+    y = mRefPoit1.imag() + y*(mRefPoit2.imag() - mRefPoit1.imag())/mRefSize;
+
+    std::complex<double> c(x,y);
+    std::complex<double> z = c;
+
+    unsigned int i(0);
+    for(i = 0; i<mMaxIters || std::norm(z)>=mEscape;i++)
+    {
+        z=pow(M_E,pow(z,3))+mJuliaValue;
+    }
+    return i;
+}
---
+++ src/FractalExspressionEvaluator.h
@@ -0,0 +1,49 @@
+#ifndef FRACTALEXSPRESSIONEVALUATOR_H
+#define FRACTALEXSPRESSIONEVALUATOR_H
+
+
+#include <complex>
+#include <cmath>
+
+class FractalExspressionEvaluator
+{
+    unsigned int mEscape;
+    unsigned int mMaxIters;
+    std::complex<double> mJuliaValue;
+
+    //these variables deal with scaling and positioning
+    unsigned int mRefSize;
+    std::complex<double> mRefPoit1;
+    std::complex<double> mRefPoit2;
+    //void* functionString = [](std::complex<double> z,const std::complex<double>& c,const std::complex<double>& j)->{};
+
+public:
+
+    FractalExspressionEvaluator();
+
+    void setPreset(unsigned int);
+
+    unsigned int GetRefSize();
+    void setRefSize(unsigned int);
+
+    unsigned int getEscapeValue();
+    void setEscapeValue(unsigned int);
+
+    unsigned int getMaxIters();
+    void setMaxIters(unsigned int);
+
+    std::complex<double> getJuliaValue();
+    void setJuliaValue(std::complex<double>);
+
+    std::complex<double> getRefPoit1();
+    void setRefPoit1(std::complex<double>);
+
+    std::complex<double> getRefPoit2();
+    void setRefPoit2(std::complex<double>);
+
+
+    int getValue(double x,double y);
+
+};
+
+#endif // FRACTALEXSPRESSIONEVALUATOR_H
